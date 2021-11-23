45e7af7d75ec4294629f98af365d3bf800ffd1a1
wangmingco <wangming.co@hotmail.com>
2021-11-23 17:16:36 +0800

    重构

--- bin/run-example.sh
+++ bin/run-example.sh
@@ -5,10 +5,14 @@ EXAMPLE_JAR=$2
 EXAMPLE_CLASS_NAME=$3
 EXAMPLE_JVM_PARAM=$4
 
+AGENT_JAR=dragonfly-agent-jar-0.1.jar
+EXAMPLE_AGENT=-javaagent:./${AGENT_JAR}
+
 echo "EXAMPLE_PROJECT: ${EXAMPLE_PROJECT}"
 echo "EXAMPLE_JAR: ${EXAMPLE_JAR}"
 echo "EXAMPLE_CLASS_NAME: ${EXAMPLE_CLASS_NAME}"
 echo "EXAMPLE_JVM_PARAM: ${EXAMPLE_JVM_PARAM}"
+echo "EXAMPLE_AGENT: ${EXAMPLE_AGENT}"
 
 cd ..
 mvn clean
@@ -17,12 +21,12 @@ mvn install
 
 cd ./dragonfly-agent
 
-cp ./dragonfly-agent-jar/target/dragonfly-agent-jar-0.1.jar ./dragonfly-agent-examples/${EXAMPLE_PROJECT}/target/dragonfly-agent-jar-0.1.jar
+cp ./dragonfly-agent-jar/target/${AGENT_JAR} ./dragonfly-agent-examples/${EXAMPLE_PROJECT}/target/${AGENT_JAR}
 
 cd ./dragonfly-agent-examples/${EXAMPLE_PROJECT}/target
 
 if [ -z "${EXAMPLE_CLASS_NAME}" ]; then
-  java -javaagent:./dragonfly-agent-jar-0.1.jar ${EXAMPLE_JVM_PARAM} -jar ./${EXAMPLE_JAR}
+  java ${EXAMPLE_AGENT} ${EXAMPLE_JVM_PARAM} -jar ./${EXAMPLE_JAR}
 else
-  java -javaagent:./dragonfly-agent-jar-0.1.jar ${EXAMPLE_JVM_PARAM} -cp ./${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME}
+  java ${EXAMPLE_AGENT} ${EXAMPLE_JVM_PARAM} -cp ./${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME}
 fi
