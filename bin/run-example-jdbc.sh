93e6b9756f01e01b0cf64ee75a2a6af28ed78510
wangmingco <wangming.co@hotmail.com>
2022-02-22 10:14:53 +0800

    升级版本

--- bin/run-example-jdbc.sh
+++ bin/run-example-jdbc.sh
@@ -4,4 +4,5 @@ EXAMPLE_PROJECT='dragonfly-example-jdbc'
 EXAMPLE_JAR='dragonfly-example-jdbc-0.1.jar'
 EXAMPLE_CLASS_NAME='example.jdbc.SimpleJdbc'
 
-sh ./run-example.sh ${EXAMPLE_PROJECT} ${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME}
+nohup sh ./run-example.sh ${EXAMPLE_PROJECT} ${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME} >nohup.log 2>&1 &
+tail -f nohup.log
--- bin/run-example-simple.sh
+++ bin/run-example-simple.sh
@@ -5,4 +5,5 @@ EXAMPLE_JAR='dragonfly-example-simple-0.1.jar'
 EXAMPLE_CLASS_NAME='example.simple.ExampleMain'
 EXAMPLE_JVM_PARAM=''
 
-sh ./run-example.sh ${EXAMPLE_PROJECT} ${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME} ${EXAMPLE_JVM_PARAM}
+nohup sh ./run-example.sh ${EXAMPLE_PROJECT} ${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME} ${EXAMPLE_JVM_PARAM} >nohup.log 2>&1 &
+tail -f nohup.log
