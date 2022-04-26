b3e71c8b580bcf65af47db2954609d61e1d81ded
wangmingco <wangming.co@hotmail.com>
2022-04-26 15:14:04 +0800

    no message

---
+++ dragonfly-agent/dragonfly-agent-core/src/main/java/co/wangming/dragonfly/agent/listener/DefaultListener.java
@@ -0,0 +1,38 @@
+package co.wangming.dragonfly.agent.listener;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.utility.JavaModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultListener implements AgentBuilder.Listener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultListener.class);
+
+    @Override
+    public void onDiscovery(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+        LOGGER.debug("onDiscovery {}", typeName);
+    }
+
+    @Override
+    public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded, DynamicType dynamicType) {
+        LOGGER.debug("onTransformation {}", typeDescription.getName());
+    }
+
+    @Override
+    public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded) {
+        LOGGER.debug("onIgnored {}", typeDescription.getName());
+    }
+
+    @Override
+    public void onError(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded, Throwable throwable) {
+        LOGGER.error("onError {}", typeName, throwable);
+    }
+
+    @Override
+    public void onComplete(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+        LOGGER.debug("onComplete {}", typeName);
+    }
+}
