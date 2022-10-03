1c7bdef25432a20dbdf68f4d21edc61088b37430
wangming <wangming.co@hotmail.com>
2022-10-03 09:19:37 +0800

    fix 日志乱码

--- bin/run-example.sh
+++ bin/run-example.sh
@@ -10,6 +10,7 @@ AGENT_JAR=dragonfly-agent-jar-0.1.jar
 EXAMPLE_AGENT=-javaagent:./${AGENT_JAR}=example
 BYTE_BUDDY_PARAMS=-Dnet.bytebuddy.dump=./${CLASS_DUMP_DIR}
 DEBUG_PARAMS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=12009
+OPTIONS_PARAMS=-Dfile.encoding="UTF-8"
 
 echo "构建环境---->"
 mvn -v
@@ -41,9 +42,9 @@ mkdir ${CLASS_DUMP_DIR}
 if [ -z "${EXAMPLE_CLASS_NAME}" ]; then
   echo "执行命令: java ${EXAMPLE_AGENT} ${EXAMPLE_JVM_PARAM} ${BYTE_BUDDY_PARAMS} -jar ./${EXAMPLE_JAR}"
 
-  java ${EXAMPLE_AGENT} ${EXAMPLE_JVM_PARAM} ${BYTE_BUDDY_PARAMS} -jar ./${EXAMPLE_JAR}
+  java ${OPTIONS_PARAMS} ${EXAMPLE_AGENT} ${EXAMPLE_JVM_PARAM} ${BYTE_BUDDY_PARAMS} -jar ./${EXAMPLE_JAR}
 else
   echo "执行命令: java ${EXAMPLE_AGENT} ${EXAMPLE_JVM_PARAM} ${BYTE_BUDDY_PARAMS} -cp ./${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME}"
 
-  java ${EXAMPLE_AGENT} ${BYTE_BUDDY_PARAMS} -cp ./${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME}
+  java ${OPTIONS_PARAMS} ${EXAMPLE_AGENT} ${BYTE_BUDDY_PARAMS} -cp ./${EXAMPLE_JAR} ${EXAMPLE_CLASS_NAME}
 fi
